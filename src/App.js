1c11be75226d147849844aacaf438037c0b754e0
gokuljsparkout <gokul@sparkouttech.com>
2023-02-02 20:04:03 +0530

    jsx ex-4

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
-{/* Data types used jsx */}
+{/* Iterate through Array */}
+
 function App() {
   const topic ="React";
   const age =17;
@@ -7,14 +8,11 @@ function App() {
 
     return (
       <div id='someId'>
-        <h1 id='header'>{topic} is Awesome !!! </h1>
-        <h2>{age}</h2>
         <h2>{likes + ""}</h2>
-        <h2>{true}</h2>
-        {/* Since if else statements doesn't works, Use Ternary Operator */}
-        <h2>{String(true)}</h2>
+        {/* To iterate through array,map method is used */}
         {
-          age<18 ? <h1>Not available for license</h1> : <h1>Available for license</h1>
+          likes.map(like => <h3>{like}</h3>
+          )
         }
       </div>
     );

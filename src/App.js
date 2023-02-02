bf3da91b43f2473ddc05255d62448aba00ceb49b
gokuljsparkout <gokul@sparkouttech.com>
2023-02-02 19:37:05 +0530

    jsx ex-2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
-
-
+{/* Data types used jsx */}
 function App() {
+  const topic ="React";
+  const age =39;
+  const likes =['JSX','React','Redux'];
 
-    const jsx = <h1>Jsx is awesome !!</h1>
-    return jsx;
+    return (
+      <div id='someId'>
+        <h1 id='header'>{topic} is Awesome !!! </h1>
+        <h2>{age}</h2>
+        <h2>{likes + ""}</h2>
+        <h2>{true}</h2> {/* Boolean doesn't print anything on the web page */}
+      </div>
+    );
 }
 export default App;

0c172f294a2126f765629e6fabeb36d92f33a1d4
gokuljsparkout <gokul@sparkouttech.com>
2023-02-02 20:57:56 +0530

    jsx mini project

--- src/App.js
+++ src/App.js
@@ -1,20 +1,64 @@
 import './App.css';
-{/* Iterate through Array */}
+{/* Simple Project :1 */}
 
 function App() {
-  const topic ="React";
-  const age =17;
-  const likes =['JSX','React','Redux'];
+  const pets = [
+    {
+      name: 'Luffy',
+      type: 'cat',
+      description: 'Luffy is funny and loves to be around people',
+      skills: ['fetch', 'play dead', 'roll over'],
+      image: 'https://res.cloudinary.com/atapas/image/upload/v1604829788/cats/remi-remino-E9kVmtiqqGE-unsplash_uivwfy.jpg',
+    },
+    {
+      name: 'Zoro',
+      type: 'cat',
+      description: 'Zoro is a sweet and cuddly cat stretch a lot!',
+      skills: ['play dead', 'meow', 'purr'],
+      image: 'https://res.cloudinary.com/atapas/image/upload/v1604829829/cats/timo-volz-ZlFKIG6dApg-unsplash_x3xifk.jpg',
+    },
+    {
+      name: 'Sanji',
+      type: 'cat',
+      description: 'Sanji is a sweet and cuddly cat stretch a lot!',
+      skills: ['play dead', 'meow', 'purr'],
+      image: 'https://res.cloudinary.com/atapas/image/upload/v1604829829/cats/timo-volz-ZlFKIG6dApg-unsplash_x3xifk.jpg',
+    },
+  ];
 
-    return (
-      <div id='someId'>
-        <h2>{likes + ""}</h2>
-        {/* To iterate through array,map method is used */}
+  return(
+    <div className='App'>
+      <h1>My Pets</h1>
+      <ul className='Container'>
         {
-          likes.map(like => <h3>{like}</h3>
-          )
-        }
-      </div>
-    );
+          pets.map( (pet,index) => (
+            <li key={index}>
+              <h2>{pet.name}</h2>
+              <p>{pet.description}</p>
+              <img src={pet.image} 
+              alt={pet.name}
+              width="200px"
+              height ="200px" />
+              <ul className="skills">
+                {
+                  pet.skills.map((skill,index)=>(
+                    <li key= {skill}>
+                      {
+                        skill
+                      }
+                    </li>
+                  )
+                  )
+                }
+              </ul>
+            </li>
+
+
+          ))
+      }  
+      </ul>
+
+    </div>
+  )
 }
 export default App;

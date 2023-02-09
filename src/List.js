9fbe3e52c37963c73f799645c1918ad2b1de67cb
gokuljsparkout <gokul@sparkouttech.com>
2023-02-09 18:07:59 +0530

    List and Keys - map() & filter()

--- src/List.js
+++ src/List.js
@@ -1,16 +1,50 @@
-
-
+//Ojects containing data inside array with key:value pair
+import React from "react";
+function List(){
 const people = [
-  'Tony Stark     : Scientist',
-  'Heisenberg     : Chemist',
-  'Bin Laden      : Terrorist',
-  'Vijay Kumar    : Police Officer',
-  'Monkey D Luffy : Pirate'
-];
-
-export default function List() {
-  const listItems = people.map(person =>
-    <li>{person}</li>
-  );
-  return <ul>{listItems}</ul>;
+  {
+    name:'Tony Stark',
+    id:1,
+    profession:'Scientist',
+  },
+  {
+    name:'Heisenberg',
+    id:2,
+    profession:'Chemist',
+},
+  {  
+   name:'Bin Laden',
+   id:3,
+   profession:'Terrorist',
+},
+ {
+  name:'Vijay Kumar',
+  id:4,
+  profession:'Police Officer',
+},
+ {name:'Monkey D Luffy',
+  id:5,
+  profession:' Pirate',
 }
+];
+const chemists =people.filter(person => person.profession==='Chemist') //Used a filter method to display only Chemist
+return(
+<div>
+<h1>List and Keys</h1>
+<ul>
+  {
+   people.map((data) =>(
+   <li key={data.id}>{data.name} : {data.profession}</li>
+  ))
+   }
+</ul>
+<u1>
+  {
+    chemists.map(person=><li>The only {person.profession} is {person.name}</li>)
+  }
+    
+</u1>
+</div>
+)
+}
+export default List

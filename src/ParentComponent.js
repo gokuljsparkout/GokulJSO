97681cfe9daf426160f0a5c48d97f78b5c9900fe
gokuljsparkout <gokul@sparkouttech.com>
2023-02-22 11:00:07 +0530

    getDerivedStatefromprops

---
+++ src/ParentComponent.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import ChildComponent from './ChildComponent';
+
+//getDerivedStatefromprops Lifecycle Method
+
+class ParentComponent extends Component {
+  state = {
+    someProp: ''
+  };
+
+  handleChange = event => {
+    this.setState({
+      someProp: event.target.value
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <label>
+          Parent Component: 
+          <input type="text" value={this.state.someProp} onChange={this.handleChange} />
+        </label>
+        <ChildComponent someProp={this.state.someProp} />
+      </div>
+    );
+  }
+}
+
+export default ParentComponent;

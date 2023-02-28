70ff7b5771d5bd9ce91d5280c5fd779e2d6fb306
gokuljsparkout <gokul@sparkouttech.com>
2023-02-28 12:58:30 +0530

    Navigate along with some parameter,fetch parameter

--- src/App.js
+++ src/App.js
@@ -1,35 +1,23 @@
 import React from "react";
-import About from "./About";
-import Contact from "./Contact";
-import Home from "./Home";
-import { BrowserRouter as Router, Link, Routes, Route } from 'react-router-dom';
-//Router Example
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ItemDetails from "./ItemDetails.js";
+import ItemList from "./ItemList";
 
 function App() {
   return (
     <Router>
-      <nav>
-        <ul>
-          <li>
-            <Link to='/'>Home</Link>
-          </li>
-
-          <li>
-            <Link to='/about'>About</Link>
-          </li>
-
-          <li>
-            <Link to='/contact'>Contact</Link>
-          </li>
-        </ul>
-      </nav>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
+        <Route path="/" element={<ItemList/>}/>
+        <Route path="/item/:id" element={<ItemDetails/>}/>
       </Routes>
     </Router>
   )
 }
 
 export default App;
+
+//The colon (:) before the id parameter tells React Router that this is a dynamic parameter
+//that will be different for each item. 
+//When a user navigates to a URL that matches this path, 
+//React Router will extract the value of the id parameter from the URL and
+// pass it as a prop to the ItemDetails component.

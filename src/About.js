25c863f94c3b34d4e5f81d390c0844f48122944a
gokuljsparkout <gokul@sparkouttech.com>
2023-02-28 11:03:37 +0530

    Router Example

---
+++ src/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>Learn more about us and what we do.</p>
+    </div>
+  )
+}
+
+export default About;
--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
 import React from "react";
-import ThemeButton from "./ThemeButton";
-import ThemeProvider from "./ThemeProvider";
+import About from "./About";
+import Contact from "./Contact";
+import Home from "./Home";
+import { BrowserRouter as Router, Link, Routes, Route } from 'react-router-dom';
+//Router Example
 
-//useContext
-function App()
-{
+function App() {
   return (
-    <div className="App">
-      <ThemeProvider>
-      <ThemeButton/>
-      </ThemeProvider>
-      
-      </div>
+    <Router>
+      <nav>
+        <ul>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+
+          <li>
+            <Link to='/about'>About</Link>
+          </li>
+
+          <li>
+            <Link to='/contact'>Contact</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
   )
 }
+
 export default App;

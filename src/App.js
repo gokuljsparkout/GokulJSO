400d5237468b75fb43f89282e9e3a3294f670903
gokuljsparkout <gokul@sparkouttech.com>
2023-03-08 11:57:51 +0530

    Custom Hooks - Form Validation Example

--- src/App.js
+++ src/App.js
@@ -1,40 +1,12 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import Signup from "./Signup";
 
 function App() {
-  const [users, setUsers] = useState([]);
-
-  useEffect(() => {
-    fetchUsers();
-  }, []);
-
-  async function fetchUsers() {
-    const response = await fetch('https://jsonplaceholder.typicode.com/users');
-    const data = await response.json();
-    setUsers(data);
-  }
-
-  async function deleteUser(id) {
-    await fetch(`https://jsonplaceholder.typicode.com/users/${id}`, 
-    {
-      method: 'DELETE'
-    }); //for deleting element in the server
-    
-    const updatedUsers = users.filter(user => user.id !== id);
-    setUsers(updatedUsers); //for rendering the updated element list in the browser
-  }
-
-  return (
+  return(
     <div>
-      <h1>User List</h1>
-      <ul>
-        {users.map(user => (
-          <li key={user.id}>
-            {user.name} <button onClick={() => deleteUser(user.id)}>Delete</button>
-          </li>
-        ))}
-      </ul>
+      <Signup/>
     </div>
-  );
+  )
 }
 
 export default App;

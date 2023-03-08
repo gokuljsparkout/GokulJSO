845851dcfef4d1b685211970b8609aa10da7122e
gokuljsparkout <gokul@sparkouttech.com>
2023-03-08 11:12:22 +0530

    Http method - Delete

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
-import React from 'react';
-import Component from './Component';
+import React, { useState, useEffect } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <Component />
-    </div>
-  )
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  async function fetchUsers() {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+    const data = await response.json();
+    setUsers(data);
   }
 
+  async function deleteUser(id) {
+    await fetch(`https://jsonplaceholder.typicode.com/users/${id}`, 
+    {
+      method: 'DELETE'
+    }); //for deleting element in the server
+    
+    const updatedUsers = users.filter(user => user.id !== id);
+    setUsers(updatedUsers); //for rendering the updated element list in the browser
+  }
+
+  return (
+    <div>
+      <h1>User List</h1>
+      <ul>
+        {users.map(user => (
+          <li key={user.id}>
+            {user.name} <button onClick={() => deleteUser(user.id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
 
 export default App;

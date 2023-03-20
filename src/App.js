bb5028d1cc4fbef308dc051cae0ccb59e54b91a5
gokuljsparkout <gokul@sparkouttech.com>
2023-03-20 12:32:54 +0530

    Without useCallback

--- src/App.js
+++ src/App.js
@@ -1,12 +1,42 @@
-import React from 'react'
-import MyComponent from './MyComponent'
-
+import React, { useState, useCallback } from 'react'
+const funccount = new Set();
 const App = () => {
+ 
+ //functions are created evrytime during re render
+  const [count, setCount] = useState(0)
+  const [number, setNumber] = useState(0)
+ 
+  const incrementCounter = () => {
+    setCount(count + 1)
+  }
+  const decrementCounter = () => {
+    setCount(count - 1)
+  }
+   
+   const incrementNumber = () => {
+    setNumber(number + 1)
+  }
+   
+funccount.add(incrementCounter);
+funccount.add(decrementCounter);
+funccount.add(incrementNumber);
+alert(funccount.size);
+ 
   return (
     <div>
-      <MyComponent name="Gokul" city ="Coimbatore" />
+      Count: {count}
+      <button onClick={incrementCounter}>
+        Increase counter
+      </button>
+      <button onClick={decrementCounter}>
+         Decrease Counter
+      </button>
+      <button onClick={incrementNumber}>
+        increase number
+      </button>
     </div>
   )
 }
-
-export default App
+ 
+ 
+export default App;
